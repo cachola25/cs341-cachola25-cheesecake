0679dd17c0d9c95f9e5a09d084f5212d748f74fc
cachola25 <cachola25@up.edu>
2023-02-13 21:47:12 -0800

    Commit

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', function (req, res, next) {
+    const orders = [
+        {topping: 'cherry', quantity: 2},
+        {topping: 'plain', quantity: 6},
+        {topping: 'chocolate', quantity: 3}
+    ]
+    res.json(orders);
+});
+
 router.post('/', function (req, res, next) {
     const orders = [
         {topping: 'cherry', quantity: 2},
