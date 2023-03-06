38b3c306d6caf734a6faac1426f2a0f461b0d4a6
cachola25 <cachola25@up.edu>
2023-03-05 22:40:53 -0800

    Adding files

--- public/javascripts/cheesecake.js
+++ public/javascripts/cheesecake.js
@@ -1,86 +1,410 @@
-
 //Author: Jayven Cachola
 
 $(function () {
-    // Hide order details
-    $(".testing-js").hide();
-    $(".display-order").hide();
+  // Hide order details
+  $(".testing-js").hide();
+  $(".display-order").hide();
 
-    // Handles order button click events
-    orderButtonHandler = function (event) {
-
-      // Check if notes contain vegan
-      var notes = $("#notes").val();
-      vegan = notes.toLowerCase();
-      if (vegan.includes("vegan")) {
-        alert("Warning!! Cheesecakes contain dairy.");
-        return;
-      }
-      // Check if topping is selected and get order details
-      var toppingVal = $("input[name=topping]:checked").val();
-      if (toppingVal != undefined) {
-
-        //Hide order form and show order details
-        $(".hide-on-click").hide();
-        $(".display-order").show();
-        $("p#order-details.display-order").text("You ordered " + $("#toppingnumber").val()
-          + " " + toppingVal + " cheesecake(s).");
-        // Check if notes need to be added to the order details
-        if (notes != "") {
-          $("p#order-notes.display-order").text("Notes: " + notes);
-        }
-
-      } else {
-        // Make sure user selects a flavor
-        alert("Please select a flavor.");
-      }
+  // Handles order button click events
+  orderButtonHandler = function (event) {
+    // Check if notes contain vegan
+    var notes = $("#notes").val();
+    vegan = notes.toLowerCase();
+    if (vegan.includes("vegan")) {
+      alert("Warning!! Cheesecakes contain dairy.");
+      return;
     }
+    // Check if topping is selected and get order details
+    var toppingVal = $("input[name=topping]:checked").val();
+    if (toppingVal != undefined) {
+      //Hide order form and show order details
+      $(".hide-on-click").hide();
+      $(".display-order").show();
+      $("p#order-details.display-order").text(
+        "You ordered " +
+          $("#toppingnumber").val() +
+          " " +
+          toppingVal +
+          " cheesecake(s)."
+      );
+      // Check if notes need to be added to the order details
+      if (notes != "") {
+        $("p#order-notes.display-order").text("Notes: " + notes);
+      }
+    } else {
+      // Make sure user selects a flavor
+      alert("Please select a flavor.");
+    }
+  };
 
-    var option = "Jan";
-    // Handles dropdown click events
-    dropdownClickHandler = function (event) {
-      option = $(this).text();
-      $("#h3-button").text(option);
+  var option = "Jan";
+  // Handles dropdown click events
+  dropdownClickHandler = function (event) {
+    option = $(this).text();
+    $("#h3-button").text(option);
 
-      //Update past order details to JSON object
-      
-      $.ajax({
-        type: "POST",
-        url: "http://localhost:3000/orders",
-        // Update list if post is successful
-        success: function(response) {
-          var orderList = $("#orders-list");
-          // Remove any existing list items
-          orderList.empty(); 
-          $.each(response, function(index, order) {
-            var orderItem = $("<li>").text(order.quantity + " " + order.topping);
-            // Append the new list item to the list
-            orderList.append(orderItem); 
-          });
-        },
-        error: function(xhr, status, error) {
-          console.log("Error: " + error);
+    //Update past order details to JSON object
+    //Author: Jayven Cachola
+
+    $(function () {
+      // Hide order details
+      $(".testing-js").hide();
+      $(".display-order").hide();
+
+      // Handles order button click events
+      orderButtonHandler = function (event) {
+        // Check if notes contain vegan
+        var notes = $("#notes").val();
+        vegan = notes.toLowerCase();
+        if (vegan.includes("vegan")) {
+          alert("Warning!! Cheesecakes contain dairy.");
+          return;
         }
+        // Check if topping is selected and get order details
+        var toppingVal = $("input[name=topping]:checked").val();
+        if (toppingVal != undefined) {
+          //Hide order form and show order details
+          $(".hide-on-click").hide();
+          $(".display-order").show();
+          $("p#order-details.display-order").text(
+            "You ordered " +
+              $("#toppingnumber").val() +
+              " " +
+              toppingVal +
+              " cheesecake(s)."
+          );
+          // Check if notes need to be added to the order details
+          if (notes != "") {
+            $("p#order-notes.display-order").text("Notes: " + notes);
+          }
+        } else {
+          // Make sure user selects a flavor
+          alert("Please select a flavor.");
+        }
+      };
+
+      const h3Button = document.getElementById("h3-button");
+      $("#Jan").on("click", function () {
+        h3Button.textContent = "Jan";
+        var orderURL = "http://localhost:3000/orders/" + "Jan";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
       });
-    }
 
-    // Handles dropdown hover events
-    dropdownHoverHandler = function (event) {
-      $("#h3-button").text("Select Month");
-    }
+      $("#Feb").on("click", function () {
 
-    // Handles dropdown leave events
-    dropdownLeaveHandler = function (event) {
-      $("#h3-button").text(option);
-    }
-    // Add event listener to order button and dropdown options
-    $("#order-button").on("click", orderButtonHandler);
-    $("a.dropdown-option").on("click", dropdownClickHandler);
-    $("#h3-button").on("mouseover", dropdownHoverHandler);
-    $("#h3-button").on("mouseleave", dropdownLeaveHandler);
-    $("#dropdown-content").on("mouseover", dropdownHoverHandler);
-    $("#dropdown-content").on("mouseleave", dropdownLeaveHandler);
+        h3Button.textContent = "Feb";
+        var orderURL = "http://localhost:3000/orders/" + "Feb";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
 
-    
-    
-  });
+      $("#Mar").on("click", function () {
+
+        h3Button.textContent = "Mar";
+        var orderURL = "http://localhost:3000/orders/" + "Mar";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Apr").on("click", function () {
+
+        h3Button.textContent = "Apr";
+        var orderURL = "http://localhost:3000/orders/" + "Apr";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#May").on("click", function () {
+
+        h3Button.textContent = "May";
+        var orderURL = "http://localhost:3000/orders/" + "May";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Jun").on("click", function () {
+
+        h3utton.textContent = "Jun";
+        var orderURL = "http://localhost:3000/orders/" + "Jun";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Jul").on("click", function () {
+
+        h3Button.textContent = "Jul";
+        var orderURL = "http://localhost:3000/orders/" + "Jul";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Aug").on("click", function () {
+
+        h3Button.textContent = "Aug";
+        var orderURL = "http://localhost:3000/orders/" + "Aug";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Sep").on("click", function () {
+
+        h3Button.textContent = "Sep";
+        var orderURL = "http://localhost:3000/orders/" + "Sep";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Oct").on("click", function () {
+
+        h3Button.textContent = "Oct";
+        var orderURL = "http://localhost:3000/orders/" + "Oct";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+
+      $("#Nov").on("click", function () {
+          
+          h3Button.textContent = "Nov";
+          var orderURL = "http://localhost:3000/orders/" + "Nov";
+          $.ajax({
+            type: "POST",
+            url: orderURL,
+            dataType: "json",
+            // Update list if post is successful
+            success: function (response) {
+              console.log(response);
+              var orderList = $("#orders-list");
+              // Remove any existing list items
+              orderList.empty();
+              $.each(response, function (index, order) {
+                var orderItem = $("<li>").text(
+                  order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+                );
+                orderList.append(orderItem);
+              });
+            },
+          });
+      });
+
+      $("#Dec").on("click", function () {
+
+        h3Button.textContent = "Dec";
+        var orderURL = "http://localhost:3000/orders/" + "Dec";
+        $.ajax({
+          type: "POST",
+          url: orderURL,
+          dataType: "json",
+          // Update list if post is successful
+          success: function (response) {
+            console.log(response);
+            var orderList = $("#orders-list");
+            // Remove any existing list items
+            orderList.empty();
+            $.each(response, function (index, order) {
+              var orderItem = $("<li>").text(
+                order.QUANTITY + " " + order.TOPPING + " cheesecake(s)"
+              );
+              orderList.append(orderItem);
+            });
+          },
+        });
+      });
+      
+
+      // Handles dropdown hover events
+      dropdownHoverHandler = function (event) {
+        $("#h3-button").text("Select Month");
+      };
+
+      // Handles dropdown leave events
+      dropdownLeaveHandler = function (event) {
+        $("#h3-button").text(option);
+      };
+      // Add event listener to order button and dropdown options
+      $("#order-button").on("click", orderButtonHandler);
+      //$("a.dropdown-option").on("click", dropdownClickHandler);
+      $("#h3-button").on("mouseover", dropdownHoverHandler);
+      $("#h3-button").on("mouseleave", dropdownLeaveHandler);
+      $("#dropdown-content").on("mouseover", dropdownHoverHandler);
+      $("#dropdown-content").on("mouseleave", dropdownLeaveHandler);
+    });
+  };
+
+  // Handles dropdown hover events
+  dropdownHoverHandler = function (event) {
+    $("#h3-button").text("Select Month");
+  };
+
+  // Handles dropdown leave events
+  dropdownLeaveHandler = function (event) {
+    $("#h3-button").text(option);
+  };
+  // Add event listener to order button and dropdown options
+  $("#order-button").on("click", orderButtonHandler);
+  $("a.dropdown-option").on("click", dropdownClickHandler);
+  $("#h3-button").on("mouseover", dropdownHoverHandler);
+  $("#h3-button").on("mouseleave", dropdownLeaveHandler);
+  $("#dropdown-content").on("mouseover", dropdownHoverHandler);
+  $("#dropdown-content").on("mouseleave", dropdownLeaveHandler);
+});
--- routes/orders.js
+++ routes/orders.js
@@ -1,28 +1,168 @@
 /*Author: Jayven Cachola*/
 
-
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var dbms = require("./dbms").dbquery;
+var sql = require("mysql");
+const orders = [
+  {topping: 'cherry', quantity: 2},
+  {topping: 'plain', quantity: 6},
+  {topping: 'chocolate', quantity: 3}
+]
 
-//Prof said it is okay to have get and post
-router.get('/', function (req, res, next) {
-    // Get orders from database
-    const orders = [
-        {topping: 'cherry', quantity: 2},
-        {topping: 'plain', quantity: 6},
-        {topping: 'chocolate', quantity: 3}
-    ]
-    res.json(orders);
+
+router.post("/Jan", function (req, res, next) {
+
+  let query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'JAN' GROUP BY TOPPING";
+  dbms(query,(err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
-router.post('/', function (req, res, next) {
-    // Post orders from database
-    const orders = [
-        {topping: 'cherry', quantity: 2},
-        {topping: 'plain', quantity: 6},
-        {topping: 'chocolate', quantity: 3}
-    ]
-    res.json(orders);
+router.post("/Feb", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'FEB' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
 });
 
+router.post("/Mar", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'MAR' GROUP BY TOPPING";
+  dbms(query,  (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Apr", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'APR' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/May", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'MAY' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Jun", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'JUN' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Jul", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'JUL' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Aug", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'AUG' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Sep", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'SEP' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Oct", function (req, res, next) {
+
+  var  query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'OCT' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Nov", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'NOV' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/Dec", function (req, res, next) {
+
+  var query = "SELECT SUM(QUANTITY) AS QUANTITY, TOPPING FROM ORDERS WHERE MONTH = 'DEC' GROUP BY TOPPING";
+  dbms(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.json(result);
+    }
+  });
+});
 module.exports = router;
